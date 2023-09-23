d847a95e982df9ecfa11c133de3a864cfe0536d5
Akhirahstrong <codeclubdetroit@gmail.com>
2023-09-22 21:54:57 -0400

    Fix bugs

--- backend/functions/src/models/substitute.ts
+++ backend/functions/src/models/substitute.ts
@@ -1,11 +1,19 @@
-import * as functions from "firebase-functions";
-import express from "express";
-import cors from "cors";
-import userRouter from "../routes/userRouter";
-import substituteRouter from "../routes/substituteRouter";
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/", userRouter);
-app.use("/", substituteRouter);
-export const api = functions.https.onRequest(app);
+// import * as functions from "firebase-functions";
+// import express from "express";
+// import cors from "cors";
+// import userRouter from "../routes/userRouter";
+// import substituteRouter from "../routes/substituteRouter";
+// export const app = express();
+// app.use(cors());
+// app.use(express.json());
+// app.use("/", userRouter);
+// app.use("/", substituteRouter);
+// export const api = functions.https.onRequest(app);
+
+import { ObjectId } from "mongodb";
+
+export default interface substitute {
+  _id?: ObjectId;
+  ingredient: string;
+  subIngredient: string;
+}
